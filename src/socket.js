fc46cfe03b427b7230e9e47f127ca46e84317f57
sakthivel9963 <sakthivel9963@gmail.com>
2020-03-07 20:40:01 +0530

    chat full logic has been done

--- src/socket.js
+++ src/socket.js
@@ -1,15 +1,22 @@
 const socketIo = require('socket.io');
 
 let userDetails = [];
+const userMessage = [];
 
 function init(server) {
   const io = socketIo(server);
   io.on('connection', socket => {
+    console.log(`${socket.id} user connected`);
     socket.emit('connected', socket.id);
 
     socket.on('sendUserDetails', data => {
       userDetails.push(data);
       io.emit('getUserDetails', userDetails);
+    });
+
+    socket.on('sendMessage', data => {
+      userMessage.push(data);
+      io.emit('getMessage', data);
     });
 
     socket.on('disconnect', () => {
