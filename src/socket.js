9b7b195abc47fe046529beb4d8afe9040ee7dff6
sakthivel9963 <sakthivel9963@gmail.com>
2020-03-07 17:56:11 +0530

    user details

--- src/socket.js
+++ src/socket.js
@@ -1,13 +1,20 @@
 const socketIo = require('socket.io');
 
+let userDetails = [];
+
 function init(server) {
   const io = socketIo(server);
   io.on('connection', socket => {
-    console.log(`${socket.id} user connected`);
-
     socket.emit('connected', socket.id);
 
+    socket.on('sendUserDetails', data => {
+      userDetails.push(data);
+      io.emit('getUserDetails', userDetails);
+    });
+
     socket.on('disconnect', () => {
+      userDetails = userDetails.filter(value => value.userId !== socket.id);
+      io.emit('getUserDetails', userDetails);
       console.log(`${socket.id} user disconnected`);
     });
   });
